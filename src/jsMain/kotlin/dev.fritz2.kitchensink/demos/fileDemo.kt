5669e978587b7780dbe8962d9b9d22a92bd70e92
jamowei <jan@rexster.de>
2021-02-06 19:05:33 +0100

    multiple files component add

--- src/jsMain/kotlin/dev.fritz2.kitchensink/demos/fileDemo.kt
+++ src/jsMain/kotlin/dev.fritz2.kitchensink/demos/fileDemo.kt
@@ -11,7 +11,7 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 @ExperimentalCoroutinesApi
 fun RenderContext.fileDemo(): Div {
 
-    fun fileModal(file: FileRead) = modal {
+    fun fileModal(file: File) = modal {
         size { small }
         closeButton()
         content {
@@ -34,9 +34,32 @@ fun RenderContext.fileDemo(): Div {
         }
     }
 
+    fun filesModal(files: List<File>) = modal {
+        size { small }
+        closeButton()
+        content {
+            stackUp {
+                items {
+                    files.forEach {
+                        lineUp {
+                            items {
+                                h3 { +it.name }
+                                p { +it.type }
+                                code { +it.content.substring(0, 15) }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     val fileStore = object : RootStore<Unit>(Unit) {
-        val showFile = handle<FileRead> { _, file ->
+        val showFile = handle<File> { _, file ->
             fileModal(file)()
+        }
+        val showFiles = handle<List<File>> { _, files ->
+            filesModal(files)()
         }
     }
 
@@ -61,11 +84,67 @@ fun RenderContext.fileDemo(): Div {
         componentFrame {
             lineUp(switchLayoutSm) {
                 items {
-                    fileButton {
+                    file {
+                        button({
+                            background { color { info } }
+                        }) {
+                            icon { fromTheme { cloudUpload } }
+                        }
+                    } handledBy fileStore.showFile
+
+                    file {
+                        accept("text/plain")
+                        button {
+                            icon { fromTheme { cloudUpload } }
+                            text("Select a text file (base64)")
+                        }
+                    } handledBy fileStore.showFile
+
+                    file {
+                        encoding("utf-8")
+                        accept("text/plain")
+                        button {
+                            icon { fromTheme { cloudUpload } }
+                            text("Select a text file (utf-8)")
+                        }
+                    } handledBy fileStore.showFile
+                }
+            }
+        }
+        playground {
+            source(
+                """
+                    val fileStore = object : RootStore<Unit>(Unit) {
+                        val showFile = handle<FileRead> { _, file ->
+                            fileModal(file)()
+                        }
+                    }
+
+                     fileButton {
                         encoding("utf-8")
                         accept("text/plain")
                         text("Select a text file")
                     } handledBy fileStore.showFile
+                """
+            )
+        }
+
+        showcaseSection("Usage")
+        paragraph {
+            +"Setup your handler for a single file which gets load by the "
+            c("fileButton")
+            +" . You can specify the accepted file types (mime-types) and use encodings for parsing the content. "
+            +"By default you get the content of the selected file as base64 string."
+        }
+        componentFrame {
+            lineUp(switchLayoutSm) {
+                items {
+                    files {
+                        button {
+                            icon { fromTheme { cloudUpload } }
+                            text("Select multiple files")
+                        }
+                    } handledBy fileStore.showFiles
                 }
             }
         }
