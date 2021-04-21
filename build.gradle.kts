cd16c21c4e0bfd381f00a5fb26cf493c1e499de8
jan <jan@rexster>
2021-04-21 13:35:36 +0200

    bump to 0.9.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ kotlin {
 
         val commonMain by getting {
             dependencies {
-                implementation("dev.fritz2:components:0.10-SNAPSHOT")
+                implementation("dev.fritz2:components:0.9.2")
             }
         }
 
