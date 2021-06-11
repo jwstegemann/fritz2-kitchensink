df0994e51aac7c4bb28e16196d9429c2c4aff543
Christian Hausknecht <chausknecht@nundp.com>
2021-06-11 17:33:03 +0200

    Rework modal demo:
    - based upon changes of PR 442 of fritz2
    
    There is one section (scrolling) missing until now.
    
    The deprecated API calls within other demo pages are not yet corrected!

--- src/jsMain/kotlin/dev.fritz2.kitchensink/demos/modalDemo.kt
+++ src/jsMain/kotlin/dev.fritz2.kitchensink/demos/modalDemo.kt
@@ -1,37 +1,47 @@
 package dev.fritz2.kitchensink.demos
 
 import dev.fritz2.binding.SimpleHandler
+import dev.fritz2.binding.storeOf
 import dev.fritz2.components.*
+import dev.fritz2.components.datatable.ColumnsContext
 import dev.fritz2.dom.html.Div
 import dev.fritz2.dom.html.RenderContext
 import dev.fritz2.kitchensink.base.*
-import dev.fritz2.styling.params.BasicParams
-import dev.fritz2.styling.params.Style
+import dev.fritz2.styling.params.FlexParams
+import dev.fritz2.styling.theme.Property
 import dev.fritz2.styling.theme.Theme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.map
 
 
 @ExperimentalCoroutinesApi
 fun RenderContext.modalDemo(): Div {
 
+    val loremIpsum = """Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy  
+                        |tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. 
+                        |At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd 
+                        |gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. 
+                        |Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy 
+                        |eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam 
+                        |voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet 
+                        |clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit 
+                        |amet.""".trimMargin()
+
     // Call this once if you don't need to dynamically change overlay.
     //ModalComponent.setOverlayHandler(DefaultOverlay(OverlayMethod.CoveringEach))
 
     return contentFrame {
-        fun createDeepDialogs(count: Int, size: Style<BasicParams>): SimpleHandler<Unit> {
+        fun createDeepDialogs(count: Int, width: Property): SimpleHandler<Unit> {
             if (count < 2) {
                 return modal {
-                    size { size }
+                    width { width }
                     content {
                         h1 { +"Final Dialog" }
                     }
                 }
             } else {
-                return modal({
-//                    background { color { neutral } }
-                }) {
-                    size { size }
-                    variant { auto }
+                return modal {
+                    width { width }
                     content {
                         h1 { +"Modal Dialog" }
                         paragraph { +"Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet." }
@@ -43,7 +53,7 @@ fun RenderContext.modalDemo(): Div {
                             items {
                                 clickButton {
                                     text("Open Another Modal")
-                                } handledBy createDeepDialogs(count - 1, size)
+                                } handledBy createDeepDialogs(count - 1, width)
                             }
                         }
                     }
@@ -53,24 +63,31 @@ fun RenderContext.modalDemo(): Div {
 
         showcaseHeader("Modal Dialog")
         paragraph {
-            +"A modal dialog is a pop up window which opens as a new layer on screen. It demands the user's attention because"
-            +" the application below is not accessible until the dialog is closed. "
+            +"A modal dialog is a pop up window which opens as a new layer on screen. It demands the user's attention "
+            +"because the application below is not accessible until the dialog is closed. "
         }
 
         showcaseSection("Usage")
         paragraph {
-            +"The fritz2 default modal can be opened with a simple clickButton - just connect it to the button's click event."
+            +"The fritz2 default modal can be opened with a simple clickButton - just connect it to the button's click "
+            +"event and add some arbitrary "
+            c("content")
+            +". Without content the modal is just a blank layer."
         }
         componentFrame {
             lineUp {
                 items {
                     clickButton {
-                        text("Default Modal Dialog")
+                        text("Open Dialog")
                     } handledBy modal {
                         content {
-                            p { +"You can put any HTML element, component or structure into a modal." }
+                            p { +"You can put any HTML element or component into a modal." }
                         }
                     }
+
+                    clickButton {
+                        text("Blank Dialog")
+                    } handledBy modal { }
                 }
             }
         }
@@ -78,22 +95,51 @@ fun RenderContext.modalDemo(): Div {
             source(
                 """
                 clickButton {
-                    text("Default Modal Dialog")
+                    text("Open Dialog")
                 } handledBy modal {
                     content {
-                        p { 
-                            +"You can put any HTML element, " 
-                            +"component or structure into a modal."
-                        }
+                        p { +"You can put any HTML element or component into a modal." }
                     }
                 }
+
+                clickButton {
+                    text("Open Default Dialog")
+                } handledBy modal { }
                 """
             )
         }
 
-        showcaseSection("Sizes")
         paragraph {
-            +"There are four predefined sizes for your modal to choose from: "
+            +"The height of the modal fits to the content, so it will grow if the content gets longer."
+        }
+        componentFrame {
+            val increaser = storeOf(1)
+
+            clickButton {
+                text("Growing Content")
+            } handledBy modal {
+                content {
+                    stackUp {
+                        items {
+                            clickButton {
+                                text(increaser.data.map { if (it == 1) "Increase" else "Decrease" })
+                            }.map { if (increaser.current == 1) 3 else 1 } handledBy increaser.update
+                            increaser.data.render {
+                                repeat(it) {
+                                    p {
+                                        +loremIpsum
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        showcaseSection("Width")
+        paragraph {
+            +"There are four predefined widths for your modal to choose from: "
             c("small")
             +", "
             c("normal")
@@ -101,80 +147,206 @@ fun RenderContext.modalDemo(): Div {
             c("large")
             +", and "
             c("full")
-            +". The additional option "
-            c("variant { verticalFilled }")
-            +" creates a modal of your chosen size which uses the entire height of the viewport."
+            +"."
+        }
+        paragraph {
+            +"But you can also pass an arbitrary sizing value if this fits your needs better (try "
+            c("380px")
+            +", "
+            c("20rem")
+            +", "
+            c("90vw")
+            +", "
+            c("20%")
+            +" and so on"
+            +")"
         }
         componentFrame {
-            lineUp({
-                alignItems { start }
-            }) {
+            stackUp {
                 items {
-                    clickButton {
-                        text("Small")
-                    } handledBy createDeepDialogs(30, Theme().modal.sizes.small)
-                    clickButton {
-                        text("Normal")
-                    } handledBy createDeepDialogs(30, Theme().modal.sizes.normal)
-                    clickButton {
-                        text("Large")
-                    } handledBy createDeepDialogs(30, Theme().modal.sizes.large)
-                    clickButton {
-                        text("Full")
-                    } handledBy createDeepDialogs(30, Theme().modal.sizes.full)
+
+                    lineUp({
+                        alignItems { start }
+                    }) {
+                        items {
+                            clickButton {
+                                text("Small")
+                            } handledBy createDeepDialogs(30, ModalComponent.WidthContext.small)
+                            clickButton {
+                                text("Normal")
+                            } handledBy createDeepDialogs(30, ModalComponent.WidthContext.normal)
+                            clickButton {
+                                text("Large")
+                            } handledBy createDeepDialogs(30, ModalComponent.WidthContext.large)
+                            clickButton {
+                                text("Full")
+                            } handledBy createDeepDialogs(30, ModalComponent.WidthContext.full)
+                        }
+                    }
+                    formControl {
+                        val widthStore = storeOf("380px")
+                        label("Custom width:")
+                        inputField(value = widthStore) {
+                            events {
+                                blurs.events.map { } handledBy modal({
+                                    this as FlexParams
+                                    display { flex }
+                                    alignItems { center }
+                                    justifyContent { center }
+                                }) {
+                                    width { widthStore.current }
+                                    content {
+                                        h1 { +"width: ${widthStore.current}" }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
-
         highlight {
             source(
                 """
                 clickButton {
                     text("full")
                 } handledBy modal {
-                    size { full }
+                    width { full }
                 }
             """.trimIndent()
             )
         }
 
+        showcaseSection("Vertical Placement")
         paragraph {
-            +"In addition to the "
-            c("size")
-            +" option, fritz2 offers the variant function "
-            c("variant { verticalFilled }")
-            +" for creating a modal which uses the entire height of the viewport."
+            +"In order to place the modal along the vertical axis, the "
+            c("placement")
+            +" option can be used. The following values are available: "
+            c("top")
+            +" (default), "
+            c("center")
+            +", "
+            c("bottom")
+            +", and "
+            c("stretch")
+            +"."
         }
         componentFrame {
+            lineUp {
+                items {
+                    clickButton {
+                        text("Top")
+                    } handledBy modal {
+                        size { small }
+                        placement { top }
+                        content {
+                            p { +loremIpsum }
+                        }
+                    }
 
-            // todo next row for space issues
-            clickButton {
-                text("Small, VerticalFilled Modal")
-            } handledBy modal {
-                variant { verticalFilled }
-                size { small }
-                content {
-                    p { +"This small modal takes all vertical space within the viewport." }
+                    clickButton {
+                        text("Center")
+                    } handledBy modal {
+                        size { small }
+                        placement { center }
+                        content {
+                            p { +loremIpsum }
+                        }
+                    }
+
+                    clickButton {
+                        text("Bottom")
+                    } handledBy modal {
+                        size { small }
+                        placement { bottom }
+                        content {
+                            p { +loremIpsum }
+                        }
+                    }
                 }
             }
 
         }
-
         highlight {
             source(
                 """
                 clickButton {
-                    text("Small, VerticalFilled Modal")
+                    text("Center")
                 } handledBy modal {
-                    variant { verticalFilled }
-                    size { small }
+                    placement { center }
                     content {
-                        p { +"This small modal takes all vertical space within the viewport." }
+                        p { +"Some content..." }
                     }
                 }
                 """.trimIndent()
             )
         }
+
+        paragraph {
+            +"The "
+            c("stretch")
+            +" option enlarges the dialog vertically from top to bottom independently from the content."
+        }
+        paragraph {
+            +"This is extremely useful if there are different contents within the modal depending on user actions. "
+            +" With a "
+            em { +"stretched" }
+            +" modal, there will be no flickering on the vertical axis, as the size stays the same"
+        }
+        componentFrame {
+            stackUp {
+                items {
+                    clickButton {
+                        text("Stretch")
+                    } handledBy modal {
+                        width { small }
+                        placement { stretch }
+                        content {
+                            p { +loremIpsum }
+                        }
+                    }
+                    lineUp({
+                        alignItems { center }
+                    }) {
+                        items {
+                            val stretched = storeOf(true)
+
+                            data class Step(val heading: String, val content: String)
+
+                            val steps = listOf(
+                                Step("Step 1", loremIpsum),
+                                Step("Step 2", loremIpsum.repeat(5)),
+                                Step("Step 3", loremIpsum.repeat(3)),
+                            )
+                            val stepStore = storeOf(steps[0])
+
+                            switch(value = stretched) { label(stretched.data.map { if (it) "stretch" else "top" }) }
+                            clickButton {
+                                text("Dynamic Content")
+                            } handledBy modal {
+                                width { large }
+                                placement { if (stretched.current) stretch else top }
+                                content {
+                                    stackUp {
+                                        items {
+                                            stepStore.data.render { currentStep ->
+                                                h1 { +currentStep.heading }
+                                                p { +currentStep.content }
+                                                clickButton { text("Next Step") }.map {
+                                                    steps[(steps.indexOf(currentStep) + 1) % steps.size]
+                                                } handledBy stepStore.update
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        showcaseSection("Scrolling")
 
         showcaseSection("Layered Modals")
         paragraph { +"Modals can be stacked over one another with only the topmost modal accepting input." }
@@ -250,7 +422,6 @@ fun RenderContext.modalDemo(): Div {
                             clickButton({
                                 background { color { danger.main } }
                                 color { neutral.main }
-                                position { absolute { top { normal } } }
                                 css("transform: rotate(-20deg) translateX(-.5rem)")
                             }) {
                                 size { small }
