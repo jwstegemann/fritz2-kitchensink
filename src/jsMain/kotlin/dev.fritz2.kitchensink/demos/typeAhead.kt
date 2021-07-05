ee6e154ed22169225a4bbb0f703d7722e516de87
Christian Hausknecht <chausknecht@nundp.com>
2021-07-05 16:02:43 +0200

    Finish demo page for TypeAhead
    - add proposal section
    - add common functionality and appearance sections

--- src/jsMain/kotlin/dev.fritz2.kitchensink/demos/typeAhead.kt
+++ src/jsMain/kotlin/dev.fritz2.kitchensink/demos/typeAhead.kt
@@ -2,12 +2,20 @@ package dev.fritz2.kitchensink.demos
 
 import dev.fritz2.binding.storeOf
 import dev.fritz2.components.*
-import dev.fritz2.components.typeAhead.*
+import dev.fritz2.components.typeAhead.Proposal
+import dev.fritz2.components.typeAhead.asProposal
 import dev.fritz2.dom.html.Div
 import dev.fritz2.dom.html.RenderContext
 import dev.fritz2.kitchensink.base.*
+import dev.fritz2.remote.getBody
+import dev.fritz2.remote.http
 import dev.fritz2.styling.*
+import dev.fritz2.styling.theme.Theme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.map
+import kotlin.js.Json
 
 
 @ExperimentalCoroutinesApi
@@ -211,6 +219,7 @@ fun RenderContext.typeAheadDemo(): Div {
                     }
                     lineUp({
                         width { full }
+                        alignItems { center }
                     }) {
                         items {
                             timeout.data.render {
@@ -243,5 +252,466 @@ fun RenderContext.typeAheadDemo(): Div {
             )
         }
 
+        showcaseSection("Patterns for Proposal Functions")
+        paragraph {
+            +"The most important aspect of using a TypeAhead is the proposal function. Therefore it is essential to "
+            +"gather a solid understanding about its meaning and some common patterns that fit most use cases."
+        }
+
+        showcaseSubSection("Mapping Draft to create a List")
+        paragraph {
+            +"The proposal function is meant to react dynamically to the current user input called the "
+            em { +"draft" }
+            +" throughout the TypeAhead component. That is why the current draft is passed as parameter into the "
+            +"proposal as the signature shows: "
+            c("suspend (Flow<String>) -> Flow<List<String>>")
+        }
+        paragraph {
+            +"Let us assume there is just a static collection of options we want to provide to the user, like a "
+            c("List<String>")
+            +" as common type:"
+        }
+        highlight {
+            source(
+                """
+                val languages = listOf(
+                    "Kotlin", "Scala", "Java", "OCaml", "Haskell", "JavaScript", "Clojure",
+                    "F#", "C++", "C#", "Commodore Basic V2", "Scheme", "Befunge"
+                )                    
+                """.trimIndent()
+            )
+        }
+        paragraph {
+            +"On the way to create a valid proposal, we ignore the wrapping of "
+            c("Flow<>")
+            +" at first and start by just using a draft parameter to filter the list by querying for "
+            +"for containment:"
+        }
+        highlight {
+            source(
+                """
+                // the component itself will inject the `draft` as parameter 
+                // into the passed function
+                val filtered: (String) -> List<String> = { draft ->
+                    languages.filter { language -> language.contains(draft) }
+                }               
+                """.trimIndent()
+            )
+        }
+        paragraph {
+            +"To lift this first approach up into a "
+            c("Flow<T>")
+            +" we need to "
+            c("map")
+            +" it:"
+        }
+        highlight {
+            source(
+                """
+                val filtered: suspend (Flow<String>) -> Flow<List<String>> = {
+                    it.map { draft -> 
+                        languages.filter { language -> language.contains(draft) } 
+                    }
+                }
+                
+                // the above can be shortened by using the type alias `Proposal`
+                import dev.fritz2.components.typeAhead.Proposal
+                
+                val filtered: Proposal = {
+                    it.map { draft -> 
+                        languages.filter { language -> language.contains(draft) } 
+                    }
+                }                
+                """.trimIndent()
+            )
+        }
+        coloredBox(Theme().colors.info) {
+            +"By using typical "
+            c("Flow")
+            +" based operations on the injected draft parameter, we can create the desired "
+            c("Flow<List<String>>")
+            +"!"
+            br {}
+            +"If there is no other flow involved, just start by using "
+            c("it.map {...}")
+            +"."
+        }
+        paragraph {
+            +"As a static list of items is quite common, there is a convenience extension function on "
+            c("List<String>")
+            +" as already showed in the very first example "
+            c("asProposal()")
+            +":"
+        }
+        highlight {
+            source(
+                """
+                import dev.fritz2.components.typeAhead.asProposal
+                val filtered = languages.asProposal()
+                """.trimIndent()
+            )
+        }
+        paragraph {
+            +"Under the hood this function creates exactly the functional expression that we have developed within "
+            +"this section!"
+        }
+        paragraph {
+            +"There is only one aspect left to explore: This extension functions can be customized by passing a "
+            +"factory function to create a filter predicate: "
+            c("asProposal(predicate: (String) -> (String) -> Boolean)")
+        }
+        paragraph {
+            +"To change the default behaviour (which is checking for containment as shown above!) like checking if "
+            +"an item starts with the draft, the predicate factory can be passed like this:"
+        }
+        highlight {
+            source(
+                """
+                import dev.fritz2.components.typeAhead.asProposal
+                val filtered = languages.asProposal { draft -> 
+                    // this predicate will be passed into the `filter`
+                    // call on the list by the component
+                    { language -> language.startsWith(draft) } 
+                }
+                """.trimIndent()
+            )
+        }
+
+        showcaseSubSection("Query Remote APIs")
+        paragraph {
+            +"As typical use case instead of using a static collection of options, we might want to query a remote "
+            +"API in order to get dynamic options."
+        }
+        paragraph {
+            +"Let's keep it simple here, so we just query the well known "
+            externalLink("SWAPI", "https://swapi.dev/")
+            +"!"
+        }
+        paragraph {
+            +"We rely on the handy "
+            externalLink("http API", "https://docs.fritz2.dev/HttpCalls.html")
+            +" of fritz2 paired with some simple "
+            externalLink("JSON parsing", "https://kotlinlang.org/api/latest/jvm/stdlib/kotlin.js/-j-s-o-n/")
+            +":"
+        }
+
+        val swapiHelper = object {
+            private val baseApi = http("https://swapi.dev/api/")
+                .acceptJson()
+                .contentType("application/json")
+
+            val getPersons: Proposal = {
+                it.map { draft ->
+                    val result = baseApi.get("people/?search=$draft").getBody()
+                    buildList {
+                        (JSON.parse<Json>(result)["results"]
+                            .unsafeCast<Array<Json>>()).forEach { person ->
+                                add(person["name"] as String)
+                            }
+                    }
+                }
+            }
+
+            fun getData(category: Flow<Pair<String, String>>): Proposal = {
+                it.combine(category) { draft, (fragment, prop) ->
+                    val result = baseApi.get("$fragment/?search=$draft").getBody()
+                    buildList {
+                        (JSON.parse<Json>(result)["results"]
+                            .unsafeCast<Array<Json>>()).forEach { item ->
+                                add(item[prop] as String)
+                            }
+                    }
+                }
+            }
+        }
+
+        componentFrame {
+            val choice = storeOf("")
+            label {
+                +"Try "
+                c("Sky")
+                +" for example..."
+            }
+            typeAhead(value = choice, items = swapiHelper.getPersons) {
+            }
+            storeContentBox("Selected") {
+                choice.data.render {
+                    span { +it }
+                }
+            }
+        }
+        highlight {
+            source(
+                """
+                // encapsulate remote API
+                object SwapiHelper {
+                    // rely on fritz2's http wrapping:
+                    private val baseApi = http("https://swapi.dev/api/")
+                        .acceptJson()
+                        .contentType("application/json")
+        
+                    // create a proposal function by applying the above presented
+                    // pattern `it.map { ... }`
+                    val getPersons: Proposal = {
+                        it.map { draft ->
+                            // fire `get` request and store result
+                            val result = baseApi.get("people/?search=${'$'}draft").getBody()
+                            buildList {
+                                // simple (unsafe) JSON parsing
+                                (JSON.parse<Json>(result)["results"]
+                                    .unsafeCast<Array<Json>>()).forEach { person ->
+                                        add(person["name"] as String)
+                                    }
+                            }
+                        }
+                    }
+                }                    
+                
+                val choice = storeOf("")
+                typeAhead(value = choice, items = SwapiHelper.getPersons) { }
+                """
+            )
+        }
+
+        showcaseSubSection("Combine Draft with other Flows")
+        paragraph {
+            +"Sometimes the valid proposals might depend on the current state of some other component too."
+        }
+        paragraph {
+            +"As fritz2's components rely on the foundational store and therefore also flow concepts, it is in fact "
+            +"quite easy to combine some external flow with the injected draft flow of the proposal function:"
+        }
+        componentFrame {
+            val categories = listOf(
+                "films" to "title",
+                "people" to "name",
+                "planets" to "name",
+                "species" to "name",
+                "starships" to "name",
+                "vehicles" to "name"
+            )
+            val category = storeOf(categories.first(), "SwapiCategory")
+            val choice = storeOf("", "SwapiChoice")
+            stackUp {
+                items {
+                    formControl {
+                        label("Category")
+                        selectField(value = category, items = categories) {
+                            label { (name, _) -> name }
+                        }
+                    }
+                    category.data.render {
+                        formControl {
+                            label("Choose from ${it.first}")
+                            typeAhead(value = choice, items = swapiHelper.getData(category.data)) {
+                            }
+                        }
+                    }
+                }
+            }
+            storeContentBox("Selected") {
+                choice.data.render {
+                    span { +it }
+                }
+            }
+        }
+        highlight {
+            source(
+                """                
+                // model: we need a path fragment and a property name to query the API
+                val categories = listOf(
+                    "films" to "title", // title differs -> we need a Pair 
+                    "people" to "name",
+                    "planets" to "name",
+                    "species" to "name",
+                    "starships" to "name",
+                    "vehicles" to "name"
+                )
+                
+                // stores
+                val category = storeOf(categories.first())
+                val choice = storeOf("")
+                
+                // UI
+                selectField(value = category, items = categories) {
+                    label { (name, _) -> name }
+                }
+                category.data.render { label { +"Choose from ${'$'}{it.first}" } }                
+                typeAhead(value = choice, items = SwapiHelper.getData(category.data)) { }
+                //                                                    ^^^^^^^^^^^^^
+                //                                                    pass additional flow
+                
+                // refactor former proposal to be more generic:  
+                object SwapiHelper {
+                    // pass additional Flow with 
+                    // - category name for the API path
+                    // - JSON property name for extracting its value
+                    fun getData(category: Flow<Pair<String, String>>): Proposal = {
+                        // mix both flows together: 
+                        it.combine(category) { draft, (fragment, prop) ->
+                            // use category as path fragment
+                            val result = baseApi.get("${'$'}fragment/?search=${'$'}draft").getBody()
+                            buildList {
+                                (JSON.parse<Json>(result)["results"]
+                                    .unsafeCast<Array<Json>>()).forEach { item ->
+                                        // use property name for data extraction
+                                        add(item[prop] as String)
+                                    }
+                            }
+                        }
+                    }                
+                }                                    
+                """
+            )
+        }
+        coloredBox(Theme().colors.info) {
+            +"Use any "
+            c("Flow")
+            +" operator function to combine the flows of other components with the proposal flow!"
+            br {}
+            +"Often "
+            c("combine")
+            +" is a good starting choice!"
+        }
+
+        paragraph {
+            +"In fact the easy combination of other component's state with the proposal function has been the "
+            +"design decision the type signature of "
+            c("Proposal")
+            +" is built upon!"
+        }
+
+        showcaseSection("Sizes")
+        paragraph {
+            +"Our TypeAhead component offers three predefined sizes: "
+            c("small")
+            +", "
+            c("normal")
+            +" (default), or "
+            c("large")
+        }
+
+        componentFrame {
+            stackUp {
+                items {
+                    val choice = storeOf("")
+                    typeAhead(value = choice, items = proposals) {
+                        size { small }
+                    }
+                    typeAhead(value = choice, items = proposals) {
+                        // omitted, as `normal`` is default!
+                    }
+                    typeAhead(value = choice, items = proposals) {
+                        size { large }
+                    }
+                }
+            }
+        }
+
+        highlight {
+            source(
+                """
+                typeAhead(value = choice, items = proposals) {
+                    size { small}
+                }
+                typeAhead(value = choice, items = proposals) {
+                    // omitted, as `normal`` is default!                
+                }
+                typeAhead(value = choice, items = proposals) {
+                    size { large}
+                }
+                """
+            )
+        }
+
+        showcaseSection("Variants")
+        paragraph {
+            +"You can choose between the variants "
+            c("outline")
+            +" or "
+            c("filled")
+            +" for the TypeAhead:"
+
+        }
+        componentFrame {
+            stackUp {
+                items {
+                    val choice = storeOf("")
+                    typeAhead(value = choice, items = proposals) {
+                        // omitted, as outline is default!
+                    }
+                    typeAhead(value = choice, items = proposals) {
+                        variant { filled }
+                    }
+                }
+            }
+        }
+
+        highlight {
+            source(
+                """
+                typeAhead(value = choice, items = proposals) {
+                    // omitted, as outline is default!
+                }
+                typeAhead(value = choice, items = proposals) {
+                    variant { filled }
+                }
+                """
+            )
+        }
+
+        showcaseSection("Disabled and Readonly")
+        paragraph {
+            +"You can easily disable the component by using the "
+            c("disabled")
+            +" or "
+            c("readonly")
+            +" functions."
+        }
+        componentFrame {
+            stackUp {
+                items {
+                    val choice = storeOf("")
+                    val toggle = storeOf(true)
+                    switch(value = toggle) {
+                        label(toggle.data.map {
+                            if (it) "disabled / readonly" else "enabled"
+                        })
+                    }
+                    formControl {
+                        label("Disabled")
+                        typeAhead(value = choice, items = proposals) {
+                            disabled(toggle.data)
+                        }
+                    }
+                    formControl {
+                        label("Readonly")
+                        typeAhead(value = choice, items = proposals) {
+                            readonly(toggle.data)
+                        }
+                    }
+                }
+            }
+        }
+        highlight {
+            source(
+                """
+                // Just to show the effect better, we can enable or disable the control
+                val toggle = storeOf(true)
+                switch(value = toggle) {
+                    label(toggle.data.map {
+                        if (it) "disabled / readonly" else "enabled"
+                    })
+                }
+                
+                typeAhead(value = choice, items = proposals) {
+                    disabled(toggle.data)
+                }
+                typeAhead(value = choice, items = proposals) {
+                    readonly(toggle.data)
+                }                    
+                """
+            )
+        }
     }
 }
